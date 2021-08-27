f5da3ece1304ec9cf43168a0252b966f7214d065
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-08-27 12:31:36 +0100

    Esercizio corretto

--- pari e dispari/js/scripts.js
+++ pari e dispari/js/scripts.js
@@ -1,9 +1,8 @@
 // Inserisci un numero, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
-var numero = (prompt("Inserisci un numero"));
+var numero = parseInt(prompt("Inserisci un numero"));
 if (numero%2 ==0){
     console.log("Il numero inserito è", numero);
 }else{
-    var numero = (prompt("L'ultimo numero inserito è dispari, inserisci un altro numero"));
-    console.log("L'ultimo numero inserito è", numero);
+    console.log("Il numero è", (numero+1));
 }
