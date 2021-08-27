debcb512f96cfdc86f6052c0f8ab3f9f1d1ef538
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-08-27 13:08:51 +0100

    Esercizio da finire

---
+++ nomi e cognomi/js/scripts.js
@@ -0,0 +1,9 @@
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+// lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+var nomi = ["Andrea", "Francesco", "Mario"];
+var cognomi = ["Fiorentino", "Rossi", "Rizzo"];
+var invitati =  [];
+for(var i = 0; i <= 3; i++){
+    invitati = nomi[i] + cognomi[i];
+}    
+console.log(invitati[i]);
