25624bfd619546288c756993ca4257620201bc6b
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-08-29 22:00:43 +0100

    Esercizio completo

--- snack-4/js/scripts.js
+++ snack-4/js/scripts.js
@@ -1,9 +1,12 @@
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
 // lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
-var nomi = ["Andrea", "Francesco", "Mario"];
-var cognomi = ["Fiorentino", "Rossi", "Rizzo"];
+var nomi = ["Andrea", "Francesco", "Mario", "Pierpaolo", "Gianfranco", "Luca", "Michele"];
+var cognomi = ["Fiorentino", "Rossi", "Rizzo", "De Lorenzis", "Capasa", "Biondi", "Neri"];
 var invitati =  [];
 for(var i = 0; i <= 3; i++){
-    invitati = nomi[i] + cognomi[i];
-}    
-console.log(invitati[i]);
+    var indiceNomi = Math.floor(Math.random() * nomi.length);
+    var indiceCognomi = Math.floor(Math.random() * cognomi.length);
+    var nomeCompleto = nomi[indiceNomi] + " " + cognomi[indiceCognomi];
+    invitati.push(nomeCompleto);
+}   
+console.log(invitati);
